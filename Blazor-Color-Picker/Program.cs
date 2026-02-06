303e3286044efebccdedd45fd87e3fc44a0eff12
Christophe Peugnet <3845786+tossnet@users.noreply.github.com>
2026-02-06 15:22:14 +0100

    update demo

--- Blazor-Color-Picker/Program.cs
+++ Blazor-Color-Picker/Program.cs
@@ -1,7 +1,8 @@
 using Blazor_Color_Picker;
+using BlazorColorPicker;
+using Brism;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorColorPicker;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddColorPicker( ServiceLifetime.Singleton);
+
+builder.Services.AddBrism();
 
 await builder.Build().RunAsync();
